51010fe669b08979d61d9c9421336f2c59c4a13b
BIT student <classroom@bgit.rs>
2018-11-01 14:04:49 +0100

    Dom

---
+++ Dom/dom.js
@@ -0,0 +1,22 @@
+// function addbackgroundC() {
+//   document.getElementsByTagName("ul")[0].className = "color";
+// }
+// addbackgroundC();
+
+// function addColor() {
+//   var secondlist = document.querySelectorAll(".secondlist li");
+
+//   for (var i = 0; i < secondlist.length; i++) {
+//     secondlist[i].className = " background";
+//   }
+// }
+// addColor();
+
+function addColor() {
+  var thirdlist = document.querySelectorAll(".thirdlist li");
+
+  for (var i = 0; i < thirdlist.length; i++) {
+    thirdlist[i].className = " color";
+  }
+}
+addColor();
